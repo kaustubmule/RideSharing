e8ad06b07b2b690f47f5f0b48f666e56bedfb9b0
Kaustub <kaumule37@gmail.com>
2025-03-10 21:10:19 +0530

    eureka server and api gateway added

--- Ride-Sharing/api-gateway/src/main/java/com/example/ApiGatewayApplication.java
+++ Ride-Sharing/api-gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -3,15 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableDiscoveryClient
-public class PaymentApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
